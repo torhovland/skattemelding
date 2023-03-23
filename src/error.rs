a8f1bef8de33c26378290df5165773273f509b50
Tor Hovland <tor+github@hovland.co>
2023-03-23 09:29:12 +0100

    No caching of error message.

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,4 @@
-use axum::response::{IntoResponse, Response};
+use axum::response::{Html, IntoResponse, Response};
 use reqwest::StatusCode;
 use serde::Deserialize;
 
@@ -12,7 +12,26 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
+            Html(format!(
+                r#"<html>
+            <head>
+              <title>Skattemelding feil</title>
+              <meta
+                http-equiv="Cache-Control"
+                content="no-cache, no-store, must-revalidate"
+              />
+              <meta http-equiv="Pragma" content="no-cache" />
+              <meta http-equiv="Expires" content="0" />
+            </head>
+            <body>
+              <p>Ein feil oppstod:</p>
+              <pre>{}</pre>
+              <a href="/">Tilbake til start</a>
+            </body>
+          </html>
+          "#,
+                self.0,
+            )),
         )
             .into_response()
     }
